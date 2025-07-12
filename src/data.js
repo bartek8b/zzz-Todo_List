bdbe35a742b73d632253bb8ae86ed0c6e47f62aa
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-07-12 20:27:53 +0200

    Add getters and setters into Todo class

--- src/data.js
+++ src/data.js
@@ -22,12 +22,60 @@ export const projects = [
 ];
 
 export class Todo {
-  constructor(title, despription, dueDate, priority, notes){
-    this._title = title,
-    this._despription = despription,
-    this._dueDate = dueDate,
-    this._priority = priority,
-    this._notes = notes,
-    this._checked = false;
+  constructor(title, despription, dueDate, priority, notes) {
+    (this._title = title),
+      (this._despription = despription),
+      (this._dueDate = dueDate),
+      (this._priority = priority),
+      (this._notes = notes),
+      (this._done = false);
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set title(title) {
+    this._title = title;
+  }
+
+  get despription() {
+    return this._despription;
+  }
+
+  set despription(despription) {
+    this._despription = despription;
+  }
+
+  get dueDate() {
+    return this._dueDate;
+  }
+
+  set dueDate(dueDate) {
+    this._dueDate = dueDate;
+  }
+
+  get priority() {
+    return this._priority;
+  }
+
+  set priority(priority) {
+    this._priority = priority;
+  }
+
+  get notes() {
+    return this._notes;
+  }
+
+  set notes(notes) {
+    this._notes = notes;
+  }
+
+  get done() {
+    return this._done;
+  }
+
+  set done(done) {
+    this._done = done;
   }
 }
