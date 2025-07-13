2baba41073fa0f33b548dab89ccb5443047ff309
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-07-13 16:51:24 +0200

    Get rid of getters and setters,keep it clean

--- src/data.js
+++ src/data.js
@@ -3,14 +3,6 @@ export class Project {
     this._name = name;
   }
 
-  get name() {
-    return this._name;
-  }
-
-  set name(name) {
-    this._name = name;
-  }
-
   todos = [];
 }
 
@@ -29,53 +21,5 @@ export class Todo {
     this._priority = priority;
     this._notes = notes;
     this._done = false;
-  }
-
-  get title() {
-    return this._title;
-  }
-
-  set title(title) {
-    this._title = title;
-  }
-
-  get despription() {
-    return this._despription;
-  }
-
-  set despription(despription) {
-    this._despription = despription;
-  }
-
-  get dueDate() {
-    return this._dueDate;
-  }
-
-  set dueDate(dueDate) {
-    this._dueDate = dueDate;
-  }
-
-  get priority() {
-    return this._priority;
-  }
-
-  set priority(priority) {
-    this._priority = priority;
-  }
-
-  get notes() {
-    return this._notes;
-  }
-
-  set notes(notes) {
-    this._notes = notes;
-  }
-
-  get done() {
-    return this._done;
-  }
-
-  set done(done) {
-    this._done = done;
   }
 }
