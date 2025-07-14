1f4e31d15ff4d5d6cdcaa602daab0d23afadc72c
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-07-14 21:56:41 +0200

    Add & implement spotItem()

--- src/execution.js
+++ src/execution.js
@@ -9,6 +9,10 @@ function isProject(name) {
   return projects.some((p) => p.name === validName);
 }
 
+function spotItem(array, property, condition){
+  return array.find((e) => e[property] === condition);
+}
+
 export function createProject(name) {
   if (!isProject(name)) {
     const validName = nameValidator(name);
@@ -22,7 +26,7 @@ function editProject() {}
 
 export function deleteProject(name) {
   const projectName = nameValidator(name);
-  const toDelete = projects.find((p) => p.name === projectName);
+  const toDelete = spotItem(projects, "name", projectName);
   const index = projects.indexOf(toDelete);
   if (index !== -1) {
     projects.splice(index, 1);
@@ -37,7 +41,7 @@ export function createTodo(project, title, description, dueDate, priority) {
   for (const p of projects) {
     if (p.name === projectName) {
       const validTitle = nameValidator(title);
-      const findTodo = p.todos.find((t) => t.title === validTitle);
+      const findTodo = spotItem(p.todos, "title", validTitle);
       if (!findTodo) {
         p.todos.push(new Todo(validTitle, description, dueDate, priority));
       } else {
