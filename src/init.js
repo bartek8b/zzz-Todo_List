33f9c5fe4518cb552ae033d0a0185e288036d57a
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-08-08 13:20:45 +0200

    Add script fo demo contetnt if storage empty

--- src/init.js
+++ src/init.js
@@ -3,9 +3,11 @@ import { createProjectsList } from "./projectsListCreator.js";
 import { createGrid } from "./gridCreator.js";
 import { projects } from "./data.js";
 import { setEventListeners } from "./controller.js";
+import { loadDemoContent } from "./demo.js";
 
 export function init() {
   retrieveStorage();
+  loadDemoContent();
   ensureDefaultProject();
   createProjectsList(projects);
   createGrid(projects);
